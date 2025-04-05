36c267f96d86f45f159e89d845cb623141c16910
gargvishu2000 <45060213+gargvishu2000@users.noreply.github.com>
2025-04-06 00:13:13 +0530

    changed mongourl

--- config/mongodb.config.js
+++ config/mongodb.config.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb+srv://shubham_garg:Ipu@1754@grain-app.lgoljfk.mongodb.net/grain-app', {     
-    });
+    const conn = await mongoose.connect('process.env.MONGODB_URI');
 
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
